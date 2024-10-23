36186b26e265a0b3e94a8ee17756d82ff7ecc7e6
surrealpatty <patty@surrealsystems.ca>
2024-10-23 14:48:16 -0700

    Update package files and add users route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/profileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Route to get user profile (requires authentication)
