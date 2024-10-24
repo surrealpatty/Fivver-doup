b7be14a42679637443236ee93a95042dfdf65af4
surrealpatty <patty@surrealsystems.ca>
2024-10-24 12:03:14 -0700

    Fixed app.js and API routes

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  development: {
-      username: 'root',
-      password: 'X^SE4Jzp$qfd1Fs2qfT*',
-      database: 'fivver_doup_db',
-      host: 'localhost',
-      dialect: 'mysql',
-      jwt: {
-          secret: 'yourDevelopmentJWTSecret', // Development JWT secret
-      },
-  },
-  production: {
-      username: 'root',
-      password: 'X^SE4Jzp$qfd1Fs2qfT*',
-      database: 'fivver_doup_db',
-      host: 'localhost',
-      dialect: 'mysql',
-      jwt: {
-          secret: 'yourProductionJWTSecret', // Production JWT secret
-      },
-  },
+    development: {
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'X^SE4Jzp$qfd1Fs2qfT*',
+        database: process.env.DB_NAME || 'fivver_doup_db',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        jwt: {
+            secret: process.env.JWT_SECRET || 'yourDevelopmentJWTSecret', // Development JWT secret
+        },
+    },
+    production: {
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'X^SE4Jzp$qfd1Fs2qfT*',
+        database: process.env.DB_NAME || 'fivver_doup_db',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        jwt: {
+            secret: process.env.JWT_SECRET || 'yourProductionJWTSecret', // Production JWT secret
+        },
+    },
 };
