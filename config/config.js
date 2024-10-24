9c88593afcaaebd5841458cc40a190bc0f960088
surrealpatty <patty@surrealsystems.ca>
2024-10-24 10:05:32 -0700

    Fixed Service model and JWT authentication issue

--- config/config.js
+++ config/config.js
@@ -1,19 +1,22 @@
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'root', // Use environment variable or default to 'root'
-    password: process.env.DB_PASSWORD || 'X^SE4Jzp$qfd1Fs2qfT*', // Environment variable for the password
-    database: process.env.DB_NAME || 'fivver_doup_db', // Environment variable or default to database name
-    host: process.env.DB_HOST || 'localhost', // Environment variable for the host
-    dialect: 'mysql', // Database dialect
+      username: 'root',
+      password: 'X^SE4Jzp$qfd1Fs2qfT*',
+      database: 'fivver_doup_db',
+      host: 'localhost',
+      dialect: 'mysql',
+      jwt: {
+          secret: 'yourDevelopmentJWTSecret', // Development JWT secret
+      },
   },
   production: {
-    username: process.env.DB_USERNAME || 'root', // Same for production environment
-    password: process.env.DB_PASSWORD || 'X^SE4Jzp$qfd1Fs2qfT*',
-    database: process.env.DB_NAME || 'fivver_doup_db',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+      username: 'root',
+      password: 'X^SE4Jzp$qfd1Fs2qfT*',
+      database: 'fivver_doup_db',
+      host: 'localhost',
+      dialect: 'mysql',
+      jwt: {
+          secret: 'yourProductionJWTSecret', // Production JWT secret
+      },
   },
-  jwt: {
-    secret: process.env.JWT_SECRET || 'yourSuperSecretKeyHere' // Add JWT secret here for token verification
-  }
 };
