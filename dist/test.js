2cd912ef010ccc398fcbb379330bdb2eb9ba277a
surrealpatty <patty@surrealsystems.ca>
2024-10-30 00:50:00 -0700

    Updated dependencies and addressed vulnerabilities

--- dist/test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// test.ts
-const greeting = "Hello, TypeScript!";
-console.log(greeting);
---
+++ main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import App from './App.vue'; // Adjust this if your App.vue file is in a different location
+
+createApp(App).mount('#app');
