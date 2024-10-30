15096148491621ada90b86bc5383a949d8b31271
surrealpatty <patty@surrealsystems.ca>
2024-10-30 00:57:29 -0700

    Updated dependencies and addressed vulnerabilities

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+      <h1>Welcome to Fivver Doup!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
