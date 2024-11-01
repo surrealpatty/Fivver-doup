b397476c0a869dc4d3f9b76ba69614be01738269
surrealpatty <patty@surrealsystems.ca>
2024-11-01 13:07:25 -0700

    Fixed tsconfig.json configuration

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-typescript', // If you're using TypeScript
+      '@vue/babel-preset-jsx' // If you're using JSX with Vue
+    ],
+  };
+  
