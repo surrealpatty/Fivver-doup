019fdf7bd3dfdbd9f8e88af09c66baf7b7317b11
surrealpatty <patty@surrealsystems.ca>
2024-11-01 12:54:50 -0700

    Fixed tsconfig.json configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.vue$': 'vue-jest',
-      '^.+\\.js$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'vue'],
-  };
-
-  
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'vue'],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  collectCoverage: true,
+  coverageDirectory: "coverage"
+};
