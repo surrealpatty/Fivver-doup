95794a590c7df1e72cea5339a7882158349d5232
Your Name <you@example.com>
2024-11-04 11:33:32 -0800

    Update dependencies and fix vulnerabilities

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { check, validationResult } from 'express-validator';
 import database from './config/database'; // Ensure this matches the actual file path
-import database from './config/database.js';
+
 
 console.log(database);
 
