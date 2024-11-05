487571fdc08568ae7a91bbe4f663d7243757d59b
Your Name <you@example.com>
2024-11-04 16:42:51 -0800

    Update dependencies and fix vulnerabilities

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,8 +1,9 @@
-const { Model, DataTypes } = require('sequelize');
+// src/models/user.js
+import { Model, DataTypes } from 'sequelize';
 
 class User extends Model {
     static associate(models) {
-        // Define associations here, if any
+        // Define associations here if needed
     }
 }
 
@@ -12,21 +13,9 @@ const initUser = (sequelize) => {
             username: {
                 type: DataTypes.STRING,
                 allowNull: false,
-                unique: true,
                 validate: {
-                    len: {
-                        args: [3, 30],
-                        msg: 'Username must be between 3 and 30 characters long.',
-                    },
-                },
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                unique: true,
-                validate: {
-                    isEmail: {
-                        msg: 'Please provide a valid email address.',
+                    notEmpty: {
+                        msg: 'Username cannot be empty',
                     },
                 },
             },
@@ -34,33 +23,10 @@ const initUser = (sequelize) => {
                 type: DataTypes.STRING,
                 allowNull: false,
                 validate: {
-                    len: {
-                        args: [6],
-                        msg: 'Password must be at least 6 characters long.',
+                    notEmpty: {
+                        msg: 'Password cannot be empty',
                     },
                 },
-            },
-            profilePicture: {
-                type: DataTypes.STRING, // URL to the profile picture
-                allowNull: true,
-            },
-            bio: {
-                type: DataTypes.TEXT, // Short biography of the user
-                allowNull: true,
-            },
-            ratings: {
-                type: DataTypes.FLOAT,
-                allowNull: true,
-                defaultValue: 0.0,
-            },
-            reviewsCount: {
-                type: DataTypes.INTEGER,
-                allowNull: true,
-                defaultValue: 0,
-            },
-            serviceOfferings: {
-                type: DataTypes.JSONB, // Stores an array of services the user offers
-                allowNull: true,
             },
         },
         {
@@ -73,7 +39,5 @@ const initUser = (sequelize) => {
     );
 };
 
-module.exports = {
-    init: initUser,
-    Model: User,
-};
+// Export the User model and the initUser function
+export { initUser, User };
