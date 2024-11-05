245798964a067e07aceb8d2c3eabc06d542a68df
Your Name <you@example.com>
2024-11-04 21:44:45 -0800

    Update dependencies and fix vulnerabilities

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,26 +1,26 @@
-import { config } from 'dotenv'; // Import the dotenv package
-config(); // Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
     development: {
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'fivver_doup_db',
-        host: process.env.DB_HOST || 'localhost',
-        dialect: process.env.DB_DIALECT || 'mysql',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
     },
     test: {
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: 'fivver_doup_db_test',
-        host: process.env.DB_HOST || 'localhost',
-        dialect: process.env.DB_DIALECT || 'mysql',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
     },
     production: {
-        username: process.env.PROD_DB_USER || 'root',
-        password: process.env.PROD_DB_PASSWORD || '',
-        database: process.env.PROD_DB_NAME || 'fivver_doup_db',
-        host: process.env.PROD_DB_HOST || 'localhost',
-        dialect: process.env.DB_DIALECT || 'mysql',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
     },
 };
