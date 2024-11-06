e41adeaf5c7fb4637c0c2f1b8c8e405244a126c9
Your Name <you@example.com>
2024-11-06 13:53:44 -0800

    Fix merge conflict and clean up .gitignore

--- Backend/jest.config.mjs
+++
@@ -1,9 +0,0 @@
-// jest.config.mjs
-export default {
-    testEnvironment: 'node',
-    testMatch: [
-      "**/tests/**/*.test.js",  // Match test files with the .test.js extension
-      "**/tests/**/*.spec.js",  // Match test files with the .spec.js extension
-    ]
-  };
-  
