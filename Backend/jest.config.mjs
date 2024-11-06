ac8499667c446ec3d6678c9faedbd69f09b4992e
Your Name <you@example.com>
2024-11-06 11:03:58 -0800

    Fix merge conflict and clean up .gitignore

--- Backend/jest.config.mjs
+++ Backend/jest.config.mjs
@@ -1,6 +1,9 @@
 // jest.config.mjs
 export default {
-    testEnvironment: 'node', // Optional, but useful if your tests are Node-based
-    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+    testEnvironment: 'node',
+    testMatch: [
+      "**/tests/**/*.[jt]s?(x)", // This will match any test files in the 'tests' directory
+      "**/?(*.)+(spec|test).[tj]s?(x)" // This will match files ending with .test.js or .spec.js
+    ],
   };
   
