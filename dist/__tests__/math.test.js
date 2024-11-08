0b34d3e9589172576c57777463e2aab58fab47eb
Your Name <you@example.com>
2024-11-08 10:57:54 -0800

    Fix merge conflict and clean up .gitignore

--- dist/__tests__/math.test.js
+++ dist/__tests__/math.test.js
@@ -1,18 +1,14 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// math.test.ts
-const math_1 = __importDefault(require("./math")); // Use ES module syntax if using TypeScript
+// dist/__tests__/math.test.js
+const math = require('./math.js'); // Ensure that math.js is in the correct location (dist/__tests__)
+
 describe('Math Functions', () => {
     test('multiplies 2 by 3 to equal 6', () => {
-        expect((0, math_1.default)(2, 3)).toBe(6);
+        expect(math(2, 3)).toBe(6);
     });
     test('multiplies -1 by 1 to equal -1', () => {
-        expect((0, math_1.default)(-1, 1)).toBe(-1);
+        expect(math(-1, 1)).toBe(-1);
     });
     test('multiplies 0 by 5 to equal 0', () => {
-        expect((0, math_1.default)(0, 5)).toBe(0);
+        expect(math(0, 5)).toBe(0);
     });
 });
