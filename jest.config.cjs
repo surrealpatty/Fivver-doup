052d159d0ae3194b02efdaa28bbb2225b7e33048
Your Name <you@example.com>
2024-11-07 19:54:16 -0800

    Fix merge conflict and clean up .gitignore

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  // This will map any import from 'src/' to 'dist/src/' when running tests
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/dist/src/$1',
+  },
+  // Additional Jest configuration options
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+  ],
+};
