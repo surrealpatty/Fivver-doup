c5f4fae3c5422948fba977e6768b23615b055f2d
Your Name <you@example.com>
2024-11-08 10:59:46 -0800

    Fix merge conflict and clean up .gitignore

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   moduleNameMapper: {
-    '^middleware/(.*)$': '<rootDir>/dist/src/middleware/$1',      // Middleware path mapping
-    '^models/(.*)$': '<rootDir>/dist/models/$1',                  // Models path mapping
-    '^controllers/(.*)$': '<rootDir>/dist/src/controllers/$1',    // Controllers path mapping
-    '^src/(.*)$': '<rootDir>/dist/src/$1',                        // General src path mapping
+    '^middleware/(.*)$': '<rootDir>/dist/middleware/$1',  // Correct the path to the dist/middleware
+    '^models/(.*)$': '<rootDir>/dist/models/$1',
+    '^src/(.*)$': '<rootDir>/dist/src/$1',
+    '^controllers/(.*)$': '<rootDir>/dist/controllers/$1',
   },
   testEnvironment: 'node',
   transform: {
