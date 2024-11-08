1bde5297fe0a8c5e5701b93678162985f35dd78e
Your Name <you@example.com>
2024-11-08 11:29:15 -0800

    Fix merge conflict and clean up .gitignore

--- dist/__tests__/math.test.js
+++ dist/__tests__/math.test.js
@@ -1,5 +1,5 @@
 // dist/__tests__/math.test.js
-const math = require('../src/math.js');
+const math = require('../math.js');
 const Service = require('../src/models/services');
 
 describe('Math Functions', () => {
