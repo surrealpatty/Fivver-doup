2480168c987641b791e0ea9436d5b8e41301f1f7
Your Name <you@example.com>
2024-11-08 10:02:19 -0800

    Fix merge conflict and clean up .gitignore

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,7 @@
 module.exports = {
   moduleNameMapper: {
+    '^../(.*)$': '<rootDir>/dist/$1', // Fix mapping for relative paths starting with '../'
     '^src/(.*)$': '<rootDir>/dist/src/$1',
-    '^@src/(.*)$': '<rootDir>/dist/src/$1', // Alternative mapping for '@src/' imports
-    '^../(.*)$': '<rootDir>/dist/$1', // Maps relative imports starting with '../' to the dist directory
   },
   testEnvironment: 'node',
   transform: {
