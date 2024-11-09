30ce7d7e1fd4927d911e38ef8ccc45f7c5aefa8a
Your Name <you@example.com>
2024-11-09 10:20:19 -0800

    Fix merge conflict and clean up .gitignore

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Preset for compiling ES6+ down to ES5
+    '@babel/preset-typescript' // Preset for compiling TypeScript
+  ],
+  plugins: [
+    // Add any necessary plugins here
+  ]
+};
