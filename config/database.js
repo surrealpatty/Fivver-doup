649b797cf2512f964b998d34b702c2925be6aa8f
Your Name <you@example.com>
2024-11-09 15:45:31 -0800

    Fix merge conflict and clean up .gitignore

--- config/database.js
+++ config/database.js
@@ -1,17 +1,22 @@
 module.exports = {
-    development: {
-      username: 'root',  // Your MySQL username
-      password: 'your_password',  // Your MySQL password
-      database: 'fivver_doup_db',  // The correct database
-      host: 'localhost',  // Your database host (usually localhost)
-      dialect: 'mysql',
+  development: {
+    username: 'root',  // Your MySQL username
+    password: 'your_password',  // Your MySQL password
+    database: 'fivver_doup_db',  // The correct database
+    host: 'localhost',  // Your database host (usually localhost)
+    dialect: 'mysql',
+    dialectOptions: {
+      charset: 'utf8mb4',  // Charset for handling full Unicode
     },
-    production: {
-      username: 'root',
-      password: 'your_password',
-      database: 'fivver_doup_db',
-      host: 'localhost',
-      dialect: 'mysql',
+  },
+  production: {
+    username: 'root',  // Your MySQL username
+    password: 'your_password',  // Your MySQL password
+    database: 'fivver_doup_db',  // The correct database
+    host: 'localhost',  // Your database host (usually localhost)
+    dialect: 'mysql',
+    dialectOptions: {
+      charset: 'utf8mb4',  // Charset for handling full Unicode
     },
-  };
-  
+  },
+};
