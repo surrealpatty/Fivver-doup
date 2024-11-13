7bd77329314c40d7e2ae41e6d38cac39b45cb770
Your Name <you@example.com>
2024-11-13 13:18:51 -0800

    Fix merge conflict and clean up .gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,30 @@
 module.exports = {
-    setupFilesAfterEnv: [
-      "<rootDir>/jest.setup.js",  // Make sure setup file is loaded after environment setup
-    ],
-    testEnvironment: "jest-environment-jsdom",  // Use jsdom for DOM-like environment
-    moduleFileExtensions: [
-      "js",
-      "ts",
-      "json",
-      "vue",
-      "node",
-    ],
-    transform: {
-      "^.+\\.vue$": "@vue/vue3-jest",  // Transform Vue files with vue3-jest
-      "^.+\\.ts$": "ts-jest",          // Transform TypeScript files with ts-jest
-      "^.+\\.jsx?$": "babel-jest",     // Transform JS/JSX files with babel-jest
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.js",  // Ensure setup file is loaded after environment setup
+  ],
+  testEnvironment: "jest-environment-jsdom",  // Use jsdom for DOM-like environment
+  moduleFileExtensions: [
+    "js",
+    "ts",
+    "json",
+    "vue",
+    "node",
+  ],
+  transform: {
+    "^.+\\.vue$": "@vue/vue3-jest",  // Transform Vue files with vue3-jest
+    "^.+\\.ts$": "ts-jest",          // Transform TypeScript files with ts-jest
+    "^.+\\.jsx?$": "babel-jest",     // Transform JS/JSX files with babel-jest
+  },
+  verbose: true,                      // Verbose test output
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",  // Resolve paths for Vue components or other files
+  },
+  globals: {
+    "vue-jest": {
+      tsconfig: "<rootDir>/tsconfig.json",  // Use tsconfig.json for vue-jest transformation
     },
-    verbose: true,                      // Verbose test output
-    moduleNameMapper: {
-      // This is for resolving paths or any Vue component imports that Jest may have trouble with
-      "^@/(.*)$": "<rootDir>/src/$1",  // Adjust this based on how you import Vue components
-    },
-    globals: {
-      "vue-jest": {
-        tsConfig: "<rootDir>/tsconfig.json",  // Make sure tsconfig is correctly used for Vue files
-      },
-    },
-    transformIgnorePatterns: [
-      // Ignore node_modules for transformation except those containing Vue code
-      "/node_modules/(?!@vue|vue3|@vue/test-utils).+\\.js$",
-    ],
-  };
-  
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@vue|vue3|@vue/test-utils).+\\.js$",  // Allow transformation of certain node_modules
+  ],
+};
