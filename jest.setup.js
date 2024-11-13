40705c8dcad8170fafd7055adac33ac83c3ae790
Your Name <you@example.com>
2024-11-13 10:43:36 -0800

    Fix merge conflict and clean up .gitignore

--- jest.setup.js
+++ jest.setup.js
@@ -1,12 +1,12 @@
 beforeAll(() => {
-  // Make sessionStorage configurable
+  // Mock sessionStorage
   Object.defineProperty(global, 'sessionStorage', {
     value: {
-      getItem: jest.fn().mockReturnValue('mockedItem'), // Mocks the return value of getItem
-      setItem: jest.fn(), // Mocks setItem with no-op
-      removeItem: jest.fn(), // Mocks removeItem with no-op
-      clear: jest.fn(), // Mocks clear with no-op
+      getItem: jest.fn().mockReturnValue('mockedItem'),  // Mocks the return value of getItem
+      setItem: jest.fn(),  // Mocks setItem with no-op
+      removeItem: jest.fn(),  // Mocks removeItem with no-op
+      clear: jest.fn(),  // Mocks clear with no-op
     },
-    writable: true, // Allow modification of sessionStorage in tests
+    writable: true,  // Allows overwriting global sessionStorage
   });
 });
