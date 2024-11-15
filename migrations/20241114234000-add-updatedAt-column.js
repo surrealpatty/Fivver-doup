1fcd92ab58170f46d7e72e59c80746610f6f14ad
Your Name <you@example.com>
2024-11-14 20:49:13 -0800

    Fix merge conflict and clean up .gitignore

--- migrations/20241114234000-add-updatedAt-column.js
+++ migrations/20241114234000-add-updatedAt-column.js
@@ -1,17 +1,21 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'updatedAt', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    });
-  },
-
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('users', 'subscriptionStatus');
-
-  },
-};
+    up: async (queryInterface, Sequelize) => {
+      const table = await queryInterface.describeTable('users');
+      
+      // Check if the column 'updatedAt' does not exist
+      if (!table.updatedAt) {
+        // Add the updatedAt column
+        return queryInterface.addColumn('users', 'updatedAt', {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        });
+      }
+    },
+  
+    down: async (queryInterface, Sequelize) => {
+      // Remove the updatedAt column in case of rollback
+      return queryInterface.removeColumn('users', 'updatedAt');
+    }
+  };
+  
