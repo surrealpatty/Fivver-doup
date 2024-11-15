2f6221fb7ff51d89bb181ca1cdeeb9d03314819b
Your Name <you@example.com>
2024-11-15 15:00:30 -0800

    Your commit message

--- migrations/20241114233126-add-first-name-column.js
+++ migrations/20241114233126-add-first-name-column.js
@@ -1,22 +1,26 @@
 'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    // Check if the 'first_name' column exists before adding it
+    const hasColumn = await queryInterface.describeTable('users');
+    if (!hasColumn['first_name']) {
+      await queryInterface.addColumn('users', 'first_name', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+
+    // Check if the 'last_name' column exists before adding it
+    if (!hasColumn['last_name']) {
+      await queryInterface.addColumn('users', 'last_name', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('users', 'first_name');
+    await queryInterface.removeColumn('users', 'last_name');
   }
 };
