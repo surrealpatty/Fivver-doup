3d3bfbf31169de7649e1e9993c1cba17329e7967
Your Name <you@example.com>
2024-11-16 14:45:31 -0800

    Fixed additional dependency vulnerabilities

--- jest.config.ts
+++ jest.config.ts
@@ -26,6 +26,10 @@ const config: Config.InitialOptions = {
   ],
   coverageDirectory: 'coverage',  // Specify the directory where coverage reports will be stored
   coverageProvider: 'v8',  // Use v8 for faster code coverage collection
+  testPathIgnorePatterns: [
+    '/node_modules/',  // Ignore node_modules during tests
+  ],
+  verbose: true,  // Enable verbose output to see detailed test results
 };
 
 export default config;
