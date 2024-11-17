2b7d72c17aaba8e32d2cf865b8c3cdb743dbdd00
Your Name <you@example.com>
2024-11-17 13:01:08 -0800

    Fixed additional dependency vulnerabilities

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+// Use default import for the CommonJS module to be compatible with ES module scope
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginVue from "eslint-plugin-vue";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  // Use tseslint recommended config directly, instead of spreading
+  {
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+    ],
+  },
   ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
 ];
