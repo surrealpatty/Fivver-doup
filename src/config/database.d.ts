ef6b15c1c7403c4ffdee22e6789f79393450ae5e
Your Name <you@example.com>
2024-11-16 21:59:26 -0800

    Fixed additional dependency vulnerabilities

--- src/config/database.d.ts
+++ src/config/database.d.ts
@@ -1,5 +1,5 @@
 // src/config/database.d.ts
 declare module '../config/database' {
-    export const sequelize: any;  // You can replace `any` with a more specific type if needed
+    export const sequelize: any;  // You can replace 'any' with more specific types if needed
   }
   
