46b6155e90e55f9c36159ec0cd580c6a921aee63
Your Name <you@example.com>
2024-11-17 14:26:36 -0800

    Fixed additional dependency vulnerabilities

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginVue from "eslint-plugin-vue";
+import { ParserOptions } from "@typescript-eslint/parser"; // Import the TypeScript parser
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,22 +17,16 @@ export default [
   // Apply TypeScript linting rules
   {
     files: ["**/*.{ts,tsx}"],
+    parser: "@typescript-eslint/parser", // Use the TypeScript parser
     plugins: {
       "@typescript-eslint": tseslint,
-    },
-    languageOptions: {
-      parser: tseslint.parser, // Ensure TypeScript files are parsed with TypeScript parser
     },
     rules: tseslint.configs.recommended.rules, // Include the recommended TypeScript rules directly
   },
   // Vue linting configuration
   {
     files: ["**/*.vue"],
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser, // Use TypeScript parser for Vue files
-      },
-    },
+    parser: "@typescript-eslint/parser", // Use TypeScript parser for Vue files
     plugins: {
       vue: pluginVue,
     },
