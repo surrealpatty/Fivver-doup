61c8579c0a9b97fefc1549c4e3ecc8f77ee8cdb0
Your Name <you@example.com>
2024-11-17 15:42:56 -0800

    Fixed additional dependency vulnerabilities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
-module.exports = {
-    env: {
-      jest: true, // Enable Jest globals
-      node: true,
-      es2021: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jest/recommended', // if using eslint-plugin-jest
-    ],
-  };
+// .eslintrc.json
+{
+    "env"; {
+      "node"; true,
+      "jest"; true
+    }
+    "extends"; ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    "parser"; "@typescript-eslint/parser",
+    "parserOptions"; {
+      "ecmaVersion"; 2020,
+      "sourceType"; "module"
+    }
+    "plugins"; ["@typescript-eslint"],
+    "rules"; {
+      // other custom rules
+    }
+  }
   
