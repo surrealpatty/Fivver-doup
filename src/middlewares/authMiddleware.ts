0fb8fb5cd5357dc1424d24be44cd748cff8965bf
Your Name <you@example.com>
2024-11-17 12:00:19 -0800

    Fixed additional dependency vulnerabilities

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import authenticateToken from './middleware/authMiddleware';
 
 // Define an interface for the JWT payload (including the id field)
 interface CustomJwtPayload extends JwtPayload {
