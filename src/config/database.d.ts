1f9922195d71895c99e912e3179a7d44d293e14f
Your Name <you@example.com>
2024-11-18 15:07:31 -0800

    Fixed additional dependency vulnerabilities

--- src/config/database.d.ts
+++ src/config/database.d.ts
@@ -1,6 +1,11 @@
-// src/config/database.d.ts
-import { Sequelize } from 'sequelize';  // Import Sequelize types
-
-declare module '../config/database' {
-  export const sequelize: Sequelize;  // Type 'sequelize' as an instance of Sequelize
+// src/config/config.d.ts
+declare module 'src/config/config' {
+  export const JWT_SECRET: string;
+  export const DB_NAME: string;
+  export const DB_USER: string;
+  export const DB_PASSWORD: string;
+  export const DB_HOST: string;
+  export const DB_DIALECT: string;
+  export const DB_SSL: string | boolean;
+  export const NODE_ENV: string;
 }
