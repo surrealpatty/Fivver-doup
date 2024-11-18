dc24d30401af3e06f62e90a85316d0dc6ff02fe4
Your Name <you@example.com>
2024-11-18 12:16:08 -0800

    Fixed additional dependency vulnerabilities

--- src/config/config.d.ts
+++ src/config/config.d.ts
@@ -1,5 +1,12 @@
 // src/config/config.d.ts
+
 declare module '../config/config' {
   export const JWT_SECRET: string;
-  // Add other config variables if necessary
+  export const DB_NAME: string;
+  export const DB_USER: string;
+  export const DB_PASSWORD: string;
+  export const DB_HOST: string;
+  export const DB_DIALECT: string;
+  export const DB_SSL: string;
+  export const NODE_ENV: string;
 }
