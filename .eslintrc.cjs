a110a50bb8625631a1c32aa77b94e0ad2f16abe8
Your Name <you@example.com>
2024-11-18 11:52:58 -0800

    Fixed additional dependency vulnerabilities

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,49 @@
 module.exports = {
-  root: true, // Mark this as the root configuration file
-  env: {
-      node: true, // Enable Node.js globals
-      jest: true, // Enable Jest globals like describe, it, expect, etc.
-      es2020: true, // Enable ES2020 features
-  },
-  extends: [
-      'eslint:recommended', // Basic linting rules
-      'plugin:@typescript-eslint/recommended', // TypeScript linting rules
-      'plugin:jest/recommended', // Jest linting rules
-  ],
-  parser: '@typescript-eslint/parser', // Use TypeScript parser
-  parserOptions: {
-      ecmaVersion: 2020, // ECMAScript version
-      sourceType: 'module', // Allow import/export syntax
-  },
-  plugins: [
-      '@typescript-eslint', // TypeScript plugin
-      'jest', // Jest plugin
-  ],
-  rules: {
-      '@typescript-eslint/no-unused-vars': 'warn', // Warn about unused variables
-      'no-undef': 'off', // Disable no-undef, since it's handled by TypeScript
-      'no-console': 'off', // Allow console.log statements
-      'prefer-const': 'warn', // Prefer const for variables that are not reassigned
-      'no-constant-condition': 'warn', // Warn about conditions that are always true/false
-      'no-fallthrough': 'warn', // Warn about fallthrough in switch cases
-  },
-  settings: {
-      jest: {
-          version: 'detect', // Automatically detect the Jest version
-      },
-  },
-};
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname
+    },
+    plugins: ['@typescript-eslint', 'vue'],
+    extends: [
+      'airbnb-typescript/base',
+      'plugin:import/typescript',
+      'plugin:vue/vue3-recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    rules: {
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.test.ts',
+            '**/jest.config.ts'
+          ]
+        }
+      ],
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn'
+    },
+    overrides: [
+      {
+        files: ['**/*.test.ts', '**/*.test.js'],
+        env: {
+          jest: true
+        },
+        globals: {
+          jest: 'readonly',
+          describe: 'readonly',
+          it: 'readonly',
+          expect: 'readonly',
+          beforeAll: 'readonly',
+          afterAll: 'readonly',
+          beforeEach: 'readonly',
+          afterEach: 'readonly'
+        }
+      }
+    ]
+  };
+  
