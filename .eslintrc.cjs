0538596c4cdc410fe1a042fdb534f4b0f3132aa5
Your Name <you@example.com>
2024-11-18 13:06:45 -0800

    Fixed additional dependency vulnerabilities

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,49 +1,49 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      project: './tsconfig.json',
-      tsconfigRootDir: __dirname
-    },
-    plugins: ['@typescript-eslint', 'vue'],
-    extends: [
-      'airbnb-typescript/base',
-      'plugin:import/typescript',
-      'plugin:vue/vue3-recommended',
-      'plugin:@typescript-eslint/recommended'
-    ],
-    rules: {
-      'import/no-extraneous-dependencies': [
-        'error',
-        {
-          devDependencies: [
-            '**/*.test.ts',
-            '**/jest.config.ts'
-          ]
-        }
-      ],
-      'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
-    },
-    overrides: [
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:import/typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        files: ['**/*.test.ts', '**/*.test.js'],
-        env: {
-          jest: true
-        },
-        globals: {
-          jest: 'readonly',
-          describe: 'readonly',
-          it: 'readonly',
-          expect: 'readonly',
-          beforeAll: 'readonly',
-          afterAll: 'readonly',
-          beforeEach: 'readonly',
-          afterEach: 'readonly'
-        }
-      }
-    ]
-  };
-  
+        devDependencies: [
+          '**/*.test.ts',
+          '**/*.test.js',
+          '**/jest.config.ts',
+        ],
+      },
+    ],
+    'vue/multi-word-component-names': 'off', // Disabled due to your preference
+    '@typescript-eslint/no-unused-vars': 'warn', // Changed to warn to reduce noise
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.js'],
+      env: {
+        jest: true,
+      },
+      globals: {
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+      },
+    },
+  ],
+};
