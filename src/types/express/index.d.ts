0a3e438807462ef7b49cff758d387ebe160fecbd
Your Name <you@example.com>
2024-11-23 16:26:14 -0800

    Add tsconfig.tsbuildinfo to .gitignore

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,11 @@
+// src/types/express/index.d.ts
+declare namespace Express {
+    export interface Request {
+      user?: {
+        id: string;
+        email?: string;
+        username?: string;
+      };
+    }
+  }
+  
