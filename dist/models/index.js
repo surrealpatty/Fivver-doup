668c7fb9d478f5882b7a5254234f3a2292c51a07
Your Name <you@example.com>
2024-11-26 17:36:10 -0800

    Your commit message

--- dist/models/index.js
+++
@@ -1,7 +0,0 @@
-// src/models/index.ts
-import User from './user'; // Import default and named export
-import { Service } from './service';
-import Review from './review';
-import Order from './order';
-// Export models and their creation attributes
-export { User, Service, Review, Order, };
