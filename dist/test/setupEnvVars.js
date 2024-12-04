fa71db20590732e5d215433a4bcde581872dd1c2
Your Actual Name <your-email@example.com>
2024-12-03 19:04:51 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/test/setupEnvVars.js
+++
@@ -1,7 +0,0 @@
-// src/test/setupEnvVars.ts
-process.env.DB_USERNAME = 'test_user';
-process.env.DB_PASSWORD = 'test_password';
-process.env.DB_DATABASE = 'test_db';
-process.env.DB_HOST = 'localhost';
-process.env.JWT_SECRET = 'testsecret';
-//# sourceMappingURL=setupEnvVars.js.map
