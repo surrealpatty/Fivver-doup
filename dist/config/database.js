f0bc86d5412e4f2895711842c69178fc3a29183e
Your Actual Name <your-email@example.com>
2024-12-05 15:23:03 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
+exports.testConnection = exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
 // Define your database connection here
 const sequelize = new sequelize_1.Sequelize({
@@ -11,4 +11,15 @@ const sequelize = new sequelize_1.Sequelize({
     database: 'fiverr_clone', // Your database name
 });
 exports.sequelize = sequelize;
+// Function to test the database connection
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate(); // Test the database connection
+        console.log('Connection established successfully.');
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+exports.testConnection = testConnection;
 //# sourceMappingURL=database.js.map
