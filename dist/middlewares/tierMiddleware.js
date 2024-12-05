1cf219beb10376a07c7420ee614499b6930f96b8
Your Actual Name <your-email@example.com>
2024-12-04 16:47:21 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/middlewares/tierMiddleware.js
+++ dist/middlewares/tierMiddleware.js
@@ -1,14 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkTier = checkTier;
-function checkTier(tier) {
+exports.checkTier = void 0;
+// Middleware to check if the user has the required tier
+const checkTier = (tier) => {
     return (req, res, next) => {
+        // Check if the user exists and if the user has the required tier
         if (req.user?.tier !== tier) {
-            res.status(403).json({ message: 'Insufficient tier' }); // Error response
+            return res.status(403).json({ message: 'Forbidden: You need to be on the appropriate tier.' });
         }
-        else {
-            next(); // Move to next middleware if check passes
-        }
+        // If tier matches, proceed to the next middleware/handler
+        next();
     };
-}
+};
+exports.checkTier = checkTier;
 //# sourceMappingURL=tierMiddleware.js.map
