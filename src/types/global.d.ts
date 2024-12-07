0f3ba2ba2de88494988a42b798dc26f054751385
Your Actual Name <your-email@example.com>
2024-12-06 16:50:44 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,14 +1,12 @@
 // src/types/global.d.ts
 
+import { UserPayload } from '@types';  // Correctly import UserPayload from the correct path
+
 // Extend the Express namespace to include the custom `user` property in the Request object
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        id: string;
-        email: string;
-        username: string;
-      };
+      user?: UserPayload;  // Ensure user matches the UserPayload type
     }
   }
 }
