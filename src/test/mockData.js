3c80a5eeb71dbeca7f0c280471291b302fb6d0a2
Your Actual Name <your-email@example.com>
2024-12-08 10:31:05 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/test/mockData.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// src/test/mockData.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockUsers = void 0;
-exports.mockUsers = [
-    { id: '1', username: 'testuser1', email: 'test1@example.com' },
-    { id: '2', username: 'testuser2', email: 'test2@example.com' },
-];
