0cfbb9a4a7b4d4567f681fd401ebf4167be5a59c
Your Actual Name <your-email@example.com>
2024-12-10 12:32:21 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRouter = express_1.default.Router();
-// Example route handler
-userRouter.get('/', (_req, res) => {
-    return res.json({ message: 'User routes are working!' });
+// Example route handler with correct signature
+userRouter.get('/', (req, res) => {
+    res.status(200).json({ message: 'User routes are working!' });
 });
 exports.default = userRouter;
 //# sourceMappingURL=user.js.map
