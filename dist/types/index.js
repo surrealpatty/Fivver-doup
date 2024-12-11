cd5c1ce2fc22d052d704c9909ef358e0f15b0a76
Your Actual Name <your-email@example.com>
2024-12-11 14:04:05 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUser = isUser;
-// Define a type guard to validate UserPayload
-function isUser(user) {
-    return user && typeof user.id === 'string'; // Check that 'user.id' is a string (you can add more checks if necessary)
-}
-//# sourceMappingURL=index.js.map
