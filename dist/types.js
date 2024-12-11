f462219c1131b78747c5f053716582a0737c3e5d
Your Actual Name <your-email@example.com>
2024-12-11 14:08:22 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUser = isUser;
-// Define the isUser type guard to validate UserPayload
-function isUser(user) {
-    return user && typeof user.id === 'string'; // Check that 'user.id' is a string (more checks can be added if needed)
-}
-//# sourceMappingURL=types.js.map
--- dist/types/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUser = isUser;
-// Define the type guard to check if the user is present
-function isUser(req) {
-    return req.user !== undefined; // Explicitly check for undefined
-}
-//# sourceMappingURL=user.js.map
