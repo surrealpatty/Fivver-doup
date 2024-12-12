fbf0ff24f08da8a30bc646cccacb259448007d41
Your Actual Name <your-email@example.com>
2024-12-11 19:18:22 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/models/user.js
+++ dist/models/user.js
@@ -2,16 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 const sequelize_1 = require("sequelize");
-const database_1 = require("../config/database"); // Ensure you're importing sequelize from the correct path
+const database_1 = require("../config/database"); // Ensure the sequelize instance is imported
 // Define the User model
 class User extends sequelize_1.Model {
 }
 exports.User = User;
-// Initialize the User model
+// Initialize the User model with the correct types
 User.init({
     id: {
         type: sequelize_1.DataTypes.STRING,
         primaryKey: true,
+        allowNull: false,
     },
     username: {
         type: sequelize_1.DataTypes.STRING,
@@ -33,18 +34,19 @@ User.init({
     tier: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false,
+        defaultValue: 'free', // Default tier is 'free'
     },
     isVerified: {
         type: sequelize_1.DataTypes.BOOLEAN,
         allowNull: false,
-        defaultValue: false,
+        defaultValue: false, // Default value for isVerified is false
     },
     resetToken: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: true, // Allow null values for resetToken
     },
     resetTokenExpiration: {
-        type: sequelize_1.DataTypes.BIGINT, // Use BIGINT for expiration time
+        type: sequelize_1.DataTypes.BIGINT,
         allowNull: true, // Allow null values for resetTokenExpiration
     },
 }, {
