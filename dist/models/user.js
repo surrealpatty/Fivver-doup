3df5f7aebb60207fbeee72b298d2233b4342f596
Your Actual Name <your-email@example.com>
2024-12-12 21:59:29 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/models/user.js
+++ dist/models/user.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 const sequelize_1 = require("sequelize");
-const database_1 = require("@config/database");
+const database_1 = require("config/database");
 class User extends sequelize_1.Model {
 }
 exports.User = User;
@@ -14,5 +14,7 @@ User.init({
     isVerified: { type: sequelize_1.DataTypes.BOOLEAN, defaultValue: false },
     role: { type: sequelize_1.DataTypes.STRING },
     tier: { type: sequelize_1.DataTypes.INTEGER, defaultValue: 1 },
+    resetToken: { type: sequelize_1.DataTypes.STRING, allowNull: true }, // Add resetToken field
+    resetTokenExpiration: { type: sequelize_1.DataTypes.DATE, allowNull: true }, // Add resetTokenExpiration field
 }, { sequelize: database_1.sequelize, modelName: 'User' });
 //# sourceMappingURL=user.js.map
