9541a8150728376038020b60ec97ec5c9ece4428
Your Actual Name <your-email@example.com>
2024-12-13 22:25:58 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = void 0;
+exports.userRoutes = exports.User = void 0;
 const sequelize_1 = require("sequelize");
 const database_1 = require("../config/database");
 class User extends sequelize_1.Model {
@@ -53,4 +53,5 @@ User.init({
     modelName: 'User',
     tableName: 'users',
 });
+exports.userRoutes = ;
 //# sourceMappingURL=user.js.map
