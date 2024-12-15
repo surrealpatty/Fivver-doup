f46f46e068860e059d376f792194a7141160f618
Your Actual Name <your-email@example.com>
2024-12-15 14:52:37 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/models/user.js
+++ dist/models/user.js
@@ -1,33 +1,66 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
 const sequelize_1 = require("sequelize");
 const database_1 = require("../config/database");
+const uuid_1 = require("uuid");
+// Define the User model class
 class User extends sequelize_1.Model {
+    static associate(models) {
+        // Define associations here
+    }
 }
+exports.User = User;
+// Initialize the User model
 User.init({
     id: {
         type: sequelize_1.DataTypes.UUID,
-        defaultValue: sequelize_1.DataTypes.UUIDV4,
+        defaultValue: uuid_1.v4,
         primaryKey: true,
+        allowNull: false,
     },
     email: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: true, // Ensure the email is unique
+        validate: {
+            isEmail: true, // Validate the email format
+        },
+    },
+    username: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+        unique: true, // Ensure the username is unique
     },
     password: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false,
     },
-    username: {
+    role: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false,
-        unique: true,
+    },
+    tier: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+    isVerified: {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        defaultValue: false, // Default to false
+    },
+    passwordResetToken: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: true, // Can be null if not in use
+    },
+    passwordResetTokenExpiry: {
+        type: sequelize_1.DataTypes.DATE,
+        allowNull: true, // Can be null if not in use
     },
 }, {
-    sequelize: database_1.sequelize,
-    modelName: 'User',
-    tableName: 'users',
+    sequelize: database_1.sequelize, // Pass the Sequelize instance
+    modelName: 'User', // Define the model name
+    tableName: 'users', // Ensure this matches your table name
+    timestamps: true, // Automatically add createdAt and updatedAt fields
 });
 exports.default = User;
 //# sourceMappingURL=user.js.map
