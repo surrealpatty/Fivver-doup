d15d55d3d8bbd535f272b581e0dcc5231f98d910
Your Actual Name <your-email@example.com>
2024-12-18 14:49:22 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { userRoutes } from './routes/user'; // Named import for user routes
 import protectedRoutes from './routes/protectedRoute'; // Import protected routes
 import serviceRoutes from './routes/service'; // Import service routes
 import  indexRoutes  from './routes/index'; // If it's a named export
+import { CustomAuthRequest, UserPayload } from '@types';  // Alias should now work
 
 
 dotenv.config();
