afa658933c831c533c4063d521b3a667a6c75f80
Your Actual Name <your-email@example.com>
2024-12-19 11:55:18 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -2,7 +2,7 @@
 
 import { Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { CustomAuthRequest } from '@types';  // Use the path alias defined in tsconfig
+import { CustomAuthRequest } from 'types';  // Use the updated path alias
 
 // Define the interface for the decoded token payload
 interface DecodedToken {
--- src/test/orderController.ts
+++ src/test/orderController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { CustomAuthRequest } from '@types';  // Use the path alias defined in tsconfig
+import { CustomAuthRequest } from 'types';  // Use the updated path alias
 import { createOrder } from '../controllers/orderController';  // Correct import for the order controller
 import { Order } from '../models/order';  // Correct import for the Order model
 import { authenticateToken } from '../middlewares/authenticateToken';  // Correct import for middleware
