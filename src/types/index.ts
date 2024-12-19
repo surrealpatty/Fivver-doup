492f9ab717d822813f14caf73c0abcfa8f1da8f8
Your Actual Name <your-email@example.com>
2024-12-19 11:36:52 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,12 @@
 // src/types/index.ts
 import { Request } from 'express';
-
 export interface CustomAuthRequest extends Request {
   user?: {
     id: string;
     email: string;
     username: string;
-    tier: string;
-    role?: string;
+    tier: 'free' | 'paid';
+    role?: 'admin' | 'user';
   };
 }
 
