e8f0ff1006e66f791f7048a4641f4b272454158b
Your Actual Name <your-email@example.com>
2024-12-18 19:40:40 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -1,7 +1,7 @@
 // src/middlewares/authenticateToken.ts
 import { Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { CustomAuthRequest } from '../types';  // Adjust the relative path as necessary
+import { CustomAuthRequest } from '../types';  // Adjust relative path based on the file location
 
 // Define the interface for the decoded token payload
 interface DecodedToken {
