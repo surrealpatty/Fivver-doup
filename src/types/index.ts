3a68e1366e03b58e577aebe94c8a1bd8434c38d4
Your Actual Name <your-email@example.com>
2024-12-19 16:56:03 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,13 @@
+// src/types/index.ts
+
 import { Request } from 'express';
 
-// Assuming this is your UserPayload interface
 export interface UserPayload {
   id: string;
-  email: string; // Make sure email is always a string
-  username: string; // or string | undefined if username can be optional
-  role?: string;  // Optional, if role is part of the payload
+  email?: string; // Make email optional
+  username?: string; // Make username optional
 }
 
 export interface CustomAuthRequest extends Request {
-  user: UserPayload;  // user is no longer undefined
+  user?: UserPayload; // user can be undefined
 }
