9b4188a72fdcf9485e9acb8ddb2be94ed961d0df
Your Actual Name <your-email@example.com>
2024-12-21 13:50:36 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,16 +14,14 @@ export interface UserPayload {
 // JwtPayload represents the structure of the JWT token
 export interface JwtPayload {
   id: string;
-  email?: string;  // Email is optional in the JWT payload
+  email: string;  // Email is optional in the JWT payload
   username?: string;  // Username is optional in the JWT payload
   tier?: string;  // Optional tier property in JWT token
 }
 
-// CustomAuthRequest extends the standard Request interface to include an optional user
 export interface CustomAuthRequest extends Request {
-  user?: UserPayload;  // `user` can be undefined, as it's set after authentication
+  user?: UserPayload;
 }
-
 // AuthRequest extends Request for cases where the user is guaranteed to be present
 export interface AuthRequest extends Request {
   user: UserPayload;  // `user` is guaranteed to be present (e.g., after JWT verification)
