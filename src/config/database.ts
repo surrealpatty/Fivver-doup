43b30ff72a4418c95797733fdf3a38639bc2470d
Your Actual Name <your-email@example.com>
2024-12-30 19:02:29 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
 import { User } from '../models/user';  // Import the User model
 import { Order } from '../models/order';  // Import the Order model
+import  Service  from '../models/services';  // Import the Service model
 
 // Load environment variables from .env file
 dotenv.config();
@@ -10,7 +11,7 @@ dotenv.config();
 const {
   DB_HOST = 'localhost',      // Default to 'localhost' if DB_HOST is not set
   DB_USER = 'root',           // Default to 'root' if DB_USER is not set
-  DB_PASSWORD = 'X^SE4Jzp$qfd1Fs2qfT*',// Default to a specific password if DB_PASSWORD is not set
+  DB_PASSWORD = 'X^SE4Jzp$qfd1Fs2qfT*', // Default to a specific password if DB_PASSWORD is not set
   DB_NAME = 'fivver_doup',    // Default to 'fivver_doup' if DB_NAME is not set
 } = process.env;
 
@@ -21,7 +22,7 @@ const sequelize = new Sequelize({
   username: DB_USER,         // Database username
   password: DB_PASSWORD,     // Database password
   database: DB_NAME,         // Database name
-  models: [User, Order],     // Explicitly reference the models you want to use
+  models: [User, Order, Service],  // Explicitly reference the models you want to use
   logging: false,            // Disable query logging (set to true for debugging)
   define: {
     timestamps: true,         // Automatically add createdAt and updatedAt fields
