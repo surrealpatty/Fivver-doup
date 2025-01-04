5607ebc0fb9dd24d6733e023f9013acb874da1a1
Your Actual Name <your-email@example.com>
2025-01-03 16:24:31 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/test/service.test.js
+++ dist/test/service.test.js
@@ -6,8 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata"); // Add this line at the very top to ensure Sequelize decorators work
 const supertest_1 = __importDefault(require("supertest"));
 const services_1 = require("../models/services"); // Corrected relative import
-const index_1 = require("../../../index.js"); // Corrected import to use the transpiled file from dist folder
-
+const index_1 = require("dist/index"); // Corrected import to use the transpiled file from dist folder
 // Mocking the Service model methods
 jest.mock('../models/services', () => ({
     Service: {
@@ -15,7 +14,6 @@ jest.mock('../models/services', () => ({
         findOne: jest.fn(),
     },
 }));
-
 describe('Service Tests', () => {
     // Apply retry logic to all tests in this suite
     beforeEach(() => {
@@ -30,19 +28,16 @@ describe('Service Tests', () => {
                 description: 'This is a test service',
                 price: 100,
             });
-
             // Send a POST request to create service endpoint
             const response = await (0, supertest_1.default)(index_1.app).post('/api/services/create').send({
                 title: 'Test Service',
                 description: 'This is a test service',
                 price: 100,
             });
-
             // Verify the response
             expect(response.status).toBe(201); // Expecting a 201 Created status
             expect(response.body).toHaveProperty('id');
             expect(response.body.title).toBe('Test Service');
-
             // Verify that Service.create was called with the correct parameters
             expect(services_1.Service.create).toHaveBeenCalledWith({
                 title: 'Test Service',
@@ -53,13 +48,11 @@ describe('Service Tests', () => {
         it('should return an error if service creation fails', async () => {
             // Mock rejected value for Service.create
             services_1.Service.create.mockRejectedValueOnce(new Error('Service creation failed'));
-
             const response = await (0, supertest_1.default)(index_1.app).post('/api/services/create').send({
                 title: 'Test Service',
                 description: 'This is a test service',
                 price: 100,
             });
-
             expect(response.status).toBe(400); // Correcting the expected status
             expect(response.body).toHaveProperty('error', 'Service creation failed');
         });
