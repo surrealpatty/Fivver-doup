a974eb43a0ba0b2ac3a4e320a3070572879a2c65
Your Actual Name <your-email@example.com>
2025-01-04 16:48:34 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- dist/src/types/express.d.js
+++
@@ -1,5 +0,0 @@
-// src/types/express.d.ts
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
--- dist/src/types/jwt.d.js
+++
@@ -1,5 +0,0 @@
-// src/types/jwt.d.ts
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
