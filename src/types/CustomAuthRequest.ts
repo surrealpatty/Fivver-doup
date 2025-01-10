3a8bb3796ce8ab0a5833bb03c179367a5f8e7492
Your Actual Name <your-email@example.com>
2025-01-09 23:21:28 -0800

    Updated .gitignore to include dist/ and force add dist/ directory

--- src/types/CustomAuthRequest.ts
+++ src/types/CustomAuthRequest.ts
@@ -1,9 +1,7 @@
-// src/types/customAuthRequest.ts
-
 import { Request } from 'express';  // Import Request from Express
 import { UserPayload } from './index';  // Import UserPayload from src/types/index
 
-// Define the CustomAuthRequest interface that extends the Express Request interface
+// CustomAuthRequest interface where user is always defined (non-optional)
 export interface CustomAuthRequest extends Request {
-    user?: UserPayload;  // 'user' is of type UserPayload and can be optional (undefined)
+  user: UserPayload;  // CustomAuthRequest expects user to always be defined
 }
